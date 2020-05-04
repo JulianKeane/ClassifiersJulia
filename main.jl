5373eb67e58c326533faf0a3f6afc0bff41f7746
Julian Keane <julian.e.keane@gmail.com>
2020-05-04 16:12:44 -0400

    Fixed error in for loop

--- main.jl
+++ main.jl
@@ -7,12 +7,11 @@ SVMclassificiationrule(X::AbstractMatrix, w) = 2*classification_rule(X,w).-1
 score(X::AbstractMatrix, Y::AbstractArray, w) = mean(classification_rule(X,w).=Y)
 
 #initialization methods
-initWeightsLike(X::AbstractMatrix) = 0*X
+initWeightsLike(X::AbstractMatrix) = zero(X[1, :])
 
 #computational method methods
 function gradientDescent(gradFunction::Function, learning_rate, itr::Integer, X::AbstractMatrix, Y::AbstractMatrix)
-    for 1:itr
+    for i = 1:itr
         w = w-learning_rate*gradFunction(X,Y,w)
     end
 end
-
