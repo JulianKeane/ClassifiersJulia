aede6e6387451c6c65562e6e1c690c3882c10263
Julian Keane <julian.e.keane@gmail.com>
2020-05-04 17:33:52 -0400

    Starting up logistic regression

--- main.jl
+++ main.jl
@@ -1,5 +1,6 @@
 using LinearAlgebra
 using Statistics
+using SparseArrays
 
 #classification methods
 classification_rule(X::AbstractMatrix,w) = transpose(w)*x .> 0
@@ -8,6 +9,10 @@ score(X::AbstractMatrix, Y::AbstractArray, w) = mean(classification_rule(X,w).=Y
 
 #initialization methods
 initWeightsLike(X::AbstractMatrix) = zero(X[1, :])
+
+#analytic methods for logistic regression
+yind = range(1,stop=length(Y), step = 1)
+log_prob(X::AbstractMatrix, Y, w) = 1//(1+exp(-1*sparse(yind,yind,Y)*(X*w)))
 
 #computational method methods
 function gradientDescent(gradFunction::Function, learning_rate, itr::Integer, X::AbstractMatrix, Y::AbstractMatrix)
