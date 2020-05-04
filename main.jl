0ba3a888c9f3985f2fc0066b1d70c76f389fcf3c
Julian Keane <julian.e.keane@gmail.com>
2020-05-04 16:05:39 -0400

    Trying more things

--- main.jl
+++ main.jl
@@ -1,5 +1,18 @@
 using LinearAlgebra
+using Statistics
 
-classification_rule(x::AbstractMatrix,w) = transpose(w)*x .> 0
-SVMclassificiationrule(x::AbstractMatrix, w) = 2*classification_rule(x,w).-1
+#classification methods
+classification_rule(X::AbstractMatrix,w) = transpose(w)*x .> 0
+SVMclassificiationrule(X::AbstractMatrix, w) = 2*classification_rule(X,w).-1
+score(X::AbstractMatrix, Y::AbstractArray, w) = mean(classification_rule(X,w).=Y)
+
+#initialization methods
+initWeightsLike(X::AbstractMatrix) = 0*X
+
+#computational method methods
+function gradientDescent(gradFunction::Function, learning_rate, itr::Integer, X::AbstractMatrix, Y::AbstractMatrix)
+    for 1:itr
+        w = w-learning_rate*gradFunction(X,Y,w)
+    end
+end
 
