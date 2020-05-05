5f64789d25e6015d123b5f6317efb247d3270b9d
Julian Keane <julian.e.keane@gmail.com>
2020-05-04 20:09:35 -0400

    Fixing some data type errors

--- main.jl
+++ main.jl
@@ -3,20 +3,26 @@ using Statistics
 using SparseArrays
 
 #classification methods
-classification_rule(X::AbstractMatrix,w) = transpose(w)*x .> 0
-SVMclassificiationrule(X::AbstractMatrix, w) = 2*classification_rule(X,w).-1
-score(X::AbstractMatrix, Y::AbstractArray, w) = mean(classification_rule(X,w).=Y)
+classification_rule(X::Matrix,w) = mul!(transpose(w),X) .> 0
+SVMclassificiationrule(X::Matrix, w) = 2*classification_rule(X,w).-1
+score(X::Matrix, Y::AbstractArray, w) = mean(classification_rule(X,w).=Y)
 
 #initialization methods
-initWeightsLike(X::AbstractMatrix) = zero(X[1, :])
+initWeightsLike(X::Matrix) = zero(X[1, :])
 
 #analytic methods for logistic regression
-yind = range(1,stop=length(Y), step = 1)
-log_prob(X::AbstractMatrix, Y, w) = 1//(1+exp(-1*sparse(yind,yind,Y)*(X*w)))
+
+log_prob(X::Matrix, Y, w) = 1//(1+exp(-1*Diagonal(Y)*(X*w)))
 
 #computational method methods
-function gradientDescent(gradFunction::Function, learning_rate, itr::Integer, X::AbstractMatrix, Y::AbstractMatrix)
+function gradientDescent(gradFunction::Function, learning_rate, itr::Integer, X::Matrix, Y::Matrix)
     for i = 1:itr
         w = w-learning_rate*gradFunction(X,Y,w)
     end
 end
+
+function SLPFit(X,Y)
+    w = zero(X[1,:])
+end
+
+using Pkg 
